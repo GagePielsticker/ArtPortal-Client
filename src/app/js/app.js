41d275acbef98babe18ca14cc7d62f1562ba4b33
Gage Pielsticker <gagepays@gmail.com>
2022-02-15 22:32:55 -0600

    Standardized JS

--- src/app/js/app.js
+++ src/app/js/app.js
@@ -1,21 +1,20 @@
 'use strict'
 
 module.exports = client => {
+  // Sometimes client does not load in time so we scan it until it exist then start the app
+  const checkInterval = setInterval(() => {
+    if (client != null) loadApp()
+  }, 5)
 
-    //Sometimes client does not load in time so we scan it until it exist then start the app
-    let checkInterval = setInterval(() => {
-        if(client != null) loadApp()
-    }, 10)
+  async function loadApp () {
+    clearInterval(checkInterval) // Stop our loop
 
-    async function loadApp() {
-        clearInterval(checkInterval) //Stop our loop
-
-        await client.isSetupComplete()
-        .then(() => {
-           // setup is complete, ignore
-        })
-        .catch(() => {
-           //setup is incomplete, run setup
-        })
-    }
+    await client.isSetupComplete()
+      .then(() => {
+        // setup is complete, ignore
+      })
+      .catch(() => {
+        // setup is incomplete, run setup
+      })
+  }
 }
--- src/app/js/database.js
+++ src/app/js/database.js
@@ -7,11 +7,11 @@ module.exports = client => {
 
   const location = path.join(__dirname, '../../data')
 
-  //If our images json file doesnt exist create it
+  // If our images json file doesnt exist create it
   client.db.createTable('images', location, (succ, msg) => {
     log.info('Creating image table/jsonfile.')
-    
-    if(succ) {
+
+    if (succ) {
       log.info('Successfully created table/jsonfile.')
     } else {
       log.warn('Error creating table/jsonfile.')
@@ -19,11 +19,11 @@ module.exports = client => {
     }
   })
 
-  //If our settings json file doesnt exist create it
+  // If our settings json file doesnt exist create it
   client.db.createTable('settings', location, (succ, msg) => {
     log.info('Creating settings/jsonfile.')
 
-    if(succ) {
+    if (succ) {
       log.info('Successfully created settings/jsonfile.')
     } else {
       log.warn('Error creating settings/jsonfile.')
@@ -50,18 +50,16 @@ module.exports = client => {
       log.info('Running isSetupComplete()')
 
       client.db.getField('settings', location, 'setupFinished', (succ, data) => {
-
-        //If record doesnt exist, create it
-        if(!succ) {
+        // If record doesnt exist, create it
+        if (!succ) {
           log.warn('setupFinished does not exist, writing.')
-          
-          client.db.insertTableContent('settings', location, { setupFinished: false}, (succ, msg) => {
-            if(succ) log.info('Inserted setupFinished Record.')
+
+          client.db.insertTableContent('settings', location, { setupFinished: false }, (succ, msg) => {
+            if (succ) log.info('Inserted setupFinished Record.')
             else log.warn(`Failed to insert first boot record: ${msg}`)
           })
-
         } else {
-          if(data[0]) return resolve() //Setup complete already
+          if (data[0]) return resolve() // Setup complete already
         }
 
         return reject()
@@ -71,25 +69,22 @@ module.exports = client => {
 
   /**
    * Changes the entry of our setup status in the DB
-   * @param {Boolean} status 
-   * @returns 
+   * @param {Boolean} status
+   * @returns
    */
   client.changeSetupStatus = status => {
     return new Promise((resolve, reject) => {
       log.info('Running changeSetupStatus()')
 
-      client.db.updateRow('settings', location, {setupFinished : false}, {setupFinished: status}, (succ, msg) => {
-        if(succ) {
+      client.db.updateRow('settings', location, { setupFinished: false }, { setupFinished: status }, (succ, msg) => {
+        if (succ) {
           log.info('Successfully changed setupFinished status.')
           resolve()
-        }
-        else {
+        } else {
           log.warn(`Could not change setupFinished status: ${msg}`)
           reject()
         }
       })
-
     })
   }
-
 }
