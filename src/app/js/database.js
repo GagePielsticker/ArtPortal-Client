08ec89344ed518d4ea166571cec8d563de1675e6
Gage Pielsticker <gagepays@gmail.com>
2022-02-15 21:23:30 -0600

    New Database System

--- src/app/js/database.js
+++ src/app/js/database.js
@@ -1,6 +1,35 @@
 'use strict'
 
 module.exports = async client => {
+  client.db = require('electron-db')
+  const path = require('path')
+  const log = require('electron-log')
+
+  const location = path.join(__dirname, '../../data')
+
+  //If our images json file doesnt exist create it
+  client.db.createTable('images', location, (succ, msg) => {
+    log.info('Creating image table/jsonfile.')
+    
+    if(succ) {
+      log.info('Successfully created table/jsonfile.')
+    } else {
+      log.warn('Error creating table/jsonfile.')
+      log.warn(msg)
+    }
+  })
+
+  //If our settings json file doesnt exist create it
+  client.db.createTable('settings', location, (succ, msg) => {
+    log.info('Creating settings/jsonfile.')
+
+    if(succ) {
+      log.info('Successfully created settings/jsonfile.')
+    } else {
+      log.warn('Error creating settings/jsonfile.')
+      log.warn(msg)
+    }
+  })
 
   // MD5 String, tags array - adds tags to md5 entry
   client.updateTags = async (md5hash, tags) => {
@@ -12,5 +41,5 @@ module.exports = async client => {
 
   }
 
-  client.updateTags("1", ["test", "test2"])
+  //client.updateTags("1", ["test", "test2"])
 }
